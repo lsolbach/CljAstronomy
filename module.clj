87a7b32093b4f5cc9249882952d6196d9400e8a5
Ludger Solbach <soulman@soulspace.org>
2013-07-31 08:59:08 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -4,7 +4,7 @@
  :type :library
  :version "0.1.0"
  :description "A library of astronomical algorithms in clojure"
- :plugins ["global" "sdeps" "clojure" "package"]
+ :plugins ["global" "sdeps" "depsdot" "clojure" "package"]
  :dependencies [[["org.clojure" "clojure" "1.5.1"]]
                 [["org.soulspace.clj" "CljLibrary" "0.3.0"]]
                 [["org.soulspace.clj" "CljMathLibrary" "0.2.0"]]]
