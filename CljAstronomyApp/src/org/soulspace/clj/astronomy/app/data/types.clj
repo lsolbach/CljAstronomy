2d4a528c10a864cb0ed6ce0be277cb814aa27240
Ludger Solbach <soulman@soulspace.org>
2015-11-14 13:17:47 +0100

    added a types namespace for protocols and records

---
+++ CljAstronomyApp/src/org/soulspace/clj/astronomy/app/data/types.clj
@@ -0,0 +1,12 @@
+(ns org.soulspace.clj.astronomy.app.data.types)
+
+(defprotocol DeepSkyObject
+  "Protocol for objects outside our solar system."
+  ; TODO add fns
+  )
+
+(defprotocol Planet
+  "Protocol for planets."
+  ; TODO add fns
+  )
+
