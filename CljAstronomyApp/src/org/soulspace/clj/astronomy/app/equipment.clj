9cdbc27feef4f15ccb792362288cf2eace515008
Ludger Solbach <soulman@soulspace.org>
2015-11-20 23:06:18 +0100

    defined application protocols

---
+++ CljAstronomyApp/src/org/soulspace/clj/astronomy/app/equipment.clj
@@ -0,0 +1,24 @@
+(ns org.soulspace.clj.astronomy.app.equipment)
+
+(defprotocol Optic
+  ""
+  )
+
+(defprotocol Eyepiece
+  ""
+  )
+
+(defprotocol BarlowReducer
+  ""
+  )
+
+(defprotocol Filter
+  ""
+  )
+
+
+(def optics-list (ref []))
+(def eyepiece-list (ref []))
+(def barlow-list (ref []))
+(def filter-list (ref []))
+
