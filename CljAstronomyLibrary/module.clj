a9ee01c86218975b5c91a74f83e49e934aa850db
Ludger Solbach <Ludger.Solbach@msg-systems.com>
2016-04-22 19:28:06 +0200

    bumped version to 0.2.0 using clojure 1.8.0
    updated dependencies

--- CljAstronomyLibrary/module.clj
+++ CljAstronomyLibrary/module.clj
@@ -2,12 +2,14 @@
  :module "CljAstronomyLibrary"
  :project "org.soulspace.clj"
  :type :library
- :version "0.1.2"
- :description "A library of astronomical algorithms in clojure"
+ :version "0.2.0"
+ :description "A library of astronomical algorithms in clojure."
+ :scm-url "https://github.com/lsolbach/CljAstronomy"
+ :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
  :plugins [["org.soulspace.baumeister/ClojurePlugin"]
            ["org.soulspace.baumeister/ClojureTestPlugin"]
            ["org.soulspace.baumeister/PackagePlugin"]]
- :dependencies [["org.clojure/clojure, 1.7.0"]
-                ["org.soulspace.clj/CljLibrary, 0.6.1"]
-                ["org.soulspace.clj/CljMathLibrary, 0.3.0"]]
+ :dependencies [["org.clojure/clojure, 1.8.0"]
+                ["org.soulspace.clj/CljLibrary, 0.7.0"]
+                ["org.soulspace.clj/CljMathLibrary, 0.5.0"]]
  ]
