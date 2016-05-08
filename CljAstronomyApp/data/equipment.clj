bf4a746998cf93b8e64a829caeee5d9628200de4
Ludger Solbach <Ludger.Solbach@msg-systems.com>
2016-05-08 21:02:20 +0200

    added equipment data file

---
+++ CljAstronomyApp/data/equipment.clj
@@ -0,0 +1,25 @@
+{:optics [{:name "Celestron Edge HD 925" :aperture 235 :focal-length 2350 :effectiveness 95 :available true}
+          {:name "8\" Dobson" :aperture 203 :focal-length 1220 :effectiveness 85  :available true}
+          ]
+ :eyepieces [{:name "Baader Hyperion Aspheric 36mm" :zoom true :focal-length 36  :fov 72 :available true}
+             {:name "Baader Hyperion Zoom 8-24mm" :zoom true :focal-length 8  :focal-length-zoom 24 :fov 70 :available true}
+             {:name "ES 82° 4.7mm" :focal-length 4.7 :fov 82 :available true}
+             {:name "ES 82° 6.7mm" :focal-length 6.7 :fov 82 :available true}
+             {:name "ES 82° 8.8mm" :focal-length 8.8 :fov 82 :available true}
+             {:name "ES 82° 11mm" :focal-length 11 :fov 82 :available true}
+             {:name "ES 82° 14mm" :focal-length 14 :fov 82 :available true}
+             {:name "ES 82° 18mm" :focal-length 18 :fov 82 :available true}
+             {:name "ES 82° 24mm" :focal-length 24 :fov 82 :available true}
+             {:name "ES 82° 30mm" :focal-length 30 :fov 82 :available true}
+             ]
+ :barlow-reducers [{:name "University Optics" :type :barlow :factor 2.8}
+                   {:name "Baader Hyperion" :type :barlow :factor 2}
+                   ]
+ :filters [{:name "Baader UHC 2\"" :barrel 2 :available true}
+           {:name "Baader OIII 2\"" :barrel 2 :available true}
+           {:name "Baader H-beta 2\"" :barrel 2 :available true}
+           {:name "Baader UV\IR Cut 2\"" :barrel 2 :available true}
+           ]
+ :cameras [{:name "Canon EOS 600 D" :type "DLSR" :size-y 0 :size-y 0 :size-pixel 0}
+           ]
+ }
