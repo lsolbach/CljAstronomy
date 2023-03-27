c3339c9f0e0bf0ee836cacae872a58a1694340d8
Ludger Solbach <soulman@soulspace.org>
2023-03-28 01:46:12 +0200

    added dev profile with portal, added deps.edn

---
+++ CljAstronomyApp/data/dev/user.clj
@@ -0,0 +1,25 @@
+
+
+;;;
+;;; Portal data viewer
+;;;
+
+(comment
+  (require '[portal.api :as portal])
+  (def p (portal/open {:launcher :vs-code}))
+  ;(def p (portal/open))
+  (add-tap #'portal/submit)
+
+  (tap> "hello")
+  (portal/clear)
+  (tap> :world)
+  (tap> {:firstname "Donald"
+         :lastname "Duck"
+         :address {:street "Entenstr. 123"
+                   :city "Entenhausen"}})
+
+  (remove-tap #'portal/submit)
+
+  (portal/close)
+  (portal/docs)
+  )
--- CljAstronomyApp/project.clj
+++ CljAstronomyApp/project.clj
@@ -6,8 +6,10 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/core.async "1.6.673"]
                  [org.clojure/data.csv "1.0.1"]
+                 [integrant/integrant "0.8.0"]
                  [reagi/reagi "0.10.1" :exclusions [org.clojure/clojure]]
                  [org.soulspace.clj/astronomy.core "0.4.2"]
                  [org.soulspace.clj/clj.swing "0.8.0"]]
   :test-paths ["test"]
+  :profiles {:dev {:dependencies [[djblue/portal "0.37.1"]]}}
   :main org.soulspace.clj.astronomy.app.ui.swing.application)
