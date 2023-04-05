9e46084296ce2e64b3a95612316cb4496adc2177
Ludger Solbach <soulman@soulspace.org>
2023-04-05 12:54:51 +0200

    updated changelog, added spec.alpha

--- CljAstronomyApp/Changelog.md
+++ CljAstronomyApp/Changelog.md
@@ -6,6 +6,6 @@ Version 0.3.0
 * drop 'clj' from namespaces
 * merged namespaces 
 * refactoring to components
-* used more concurrency with asynchronous communication
+* implemented asynchronous communication for catalogs
 * added abstractions for catalogs and objects
 
--- CljAstronomyApp/project.clj
+++ CljAstronomyApp/project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/spec.alpha "0.3.218"]
                  [org.clojure/core.async "1.6.673"]
                  [org.clojure/data.csv "1.0.1"]
                  [integrant/integrant "0.8.0"]
