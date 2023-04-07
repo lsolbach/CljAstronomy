fe20fff2c063fa59ec6a9d57b44804ccee0f9dcf
Ludger Solbach <soulman@soulspace.org>
2023-04-07 15:58:08 +0200

    updated project files

--- CljAstronomyApp/project.clj
+++ CljAstronomyApp/project.clj
@@ -10,8 +10,9 @@
                  [integrant/integrant "0.8.0"]
                  [reagi/reagi "0.10.1" :exclusions [org.clojure/clojure]]
                  [org.soulspace.clj/astronomy.core "0.4.3-SNAPSHOT"]
-                 [org.soulspace.clj/clj.swing "0.8.0"]]
+                 [org.soulspace.clj/clj.swing "0.8.1-SNAPSHOT"]]
   :test-paths ["test"]
-  :profiles {:dev {:dependencies [[djblue/portal "0.37.1"]]
+  :profiles {:dev {:dependencies [[djblue/portal "0.37.1"]
+                                  [criterium "0.4.6"]]
                    :global-vars {*warn-on-reflection* true}}}
-  :main org.soulspace.clj.astronomy.app.ui.swing.application)
+  :main org.soulspace.astronomy.app.ui.swing.application)
